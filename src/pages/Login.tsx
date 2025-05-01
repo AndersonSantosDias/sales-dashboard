ee02970f3b61e1ad27bb28c3dfe9bd86dbc574b5
AndersonSantosDias <andersonsd.dev>
2025-05-01 20:15:56 -0300

    feat: material-ui-insert

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,25 @@
-import styled from "styled-components"
-
-const LoginArea = styled.div`
-  background: #666;
-  `
-
-const LoginImage = styled.div`
-  background-image: url(/login-image.svg);
-  background-size: cover;
-  height: 100vh;
-  width: 50vw;
-  `
+import { Box, Container, Grid } from '@mui/material'
+import { BannerImage } from '@/components'
 
 function Login() {
-    return (
-        <>
-            <LoginArea>Login</LoginArea>
-            <LoginImage/>
-        </>
-    )
+  return (
+    <>
+      <Box>
+        <Grid container>
+          <Grid
+            size={{ xs: 12, sm: 6 }}
+            sx={{ display: 'flex', alignItems: 'center', height: '100vh' }}
+          >
+            <Container maxWidth="sm">
+              <h1>Login</h1>
+            </Container>
+          </Grid>
+          <Grid size={{ sm: 6 }} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <BannerImage />
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  )
 }
 export default Login
-
